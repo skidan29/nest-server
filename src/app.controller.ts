b9596685fb3e772990f1fbfa2af22388ed75e381
skidan29 <63918439+skidan29@users.noreply.github.com>
2022-03-04 12:32:24 +0300

    Отключение Cors

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    return '<h1>Hello</h1>';
+    return '<h1>Nest API</h1>';
   }
+
   @Get('/test')
   getWorld(): string {
     return JSON.stringify('test');
