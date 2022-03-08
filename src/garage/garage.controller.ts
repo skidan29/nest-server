68cc20136753f3a8c74b934c147575a1a6472138
skidan29 <skidan2908@gmail.com>
2022-03-08 21:21:19 +0300

    Подключение к созданой БД mongo DB

--- src/garage/garage.controller.ts
+++ src/garage/garage.controller.ts
@@ -1,15 +1,51 @@
 import {
-  Controller,
-  Get,
-
+    Controller,
+    Get, Post, Res
 } from "@nestjs/common";
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const client = new MongoClient("mongodb+srv://Skidan_1306:Skidan_1306@cluster0.f8mq5.mongodb.net/test?retryWrites=true&w=majority");
+
+const getStart = async () => {
+
+  try{
+   await client.connect();
+
+   const collection =  await client.db("test").collection("garages");
+   return await collection.find().toArray();
+
+  }catch (e){
+      console.log('ошибка соединения', e);
+  }
+
+}
+
+const postStart = async () => {
+
+    try{
+        await client.connect();
+
+        const collection =  await client.db("test").collection("garages");
+        return await collection.find().toArray();
+
+    }catch (e){
+        console.log('ошибка соединения', e);
+    }
+
+}
 
 @Controller('api')
 export class GarageController {
 
   @Get('/garages')
-  getWorld(): string {
-    return JSON.stringify({ number: 1231, name: 'OB2' });
+  getWorld() {
 
+    return getStart();
   }
+
+  @Post('/garages')
+    postWorld(@Res() res) {
+
+       console.log('res', res)
+    }
 }
