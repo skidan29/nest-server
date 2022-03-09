db939df4812f1041f10464b19d7b007ac6ba5c71
skidan29 <skidan2908@gmail.com>
2022-03-09 21:51:07 +0300

    синхронизация создания новго обекта на бэкенде и фронте

--- src/garage/garage.controller.ts
+++ src/garage/garage.controller.ts
@@ -1,51 +1,42 @@
-import {
-    Controller,
-    Get, Post, Res
-} from "@nestjs/common";
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import { Controller, Get, Post, Req } from '@nestjs/common';
+import { MongoClient } from 'mongodb';
 
-const client = new MongoClient("mongodb+srv://Skidan_1306:Skidan_1306@cluster0.f8mq5.mongodb.net/test?retryWrites=true&w=majority");
+const client = new MongoClient(
+  'mongodb+srv://Skidan_1306:Skidan_1306@cluster0.f8mq5.mongodb.net/test?retryWrites=true&w=majority',
+);
 
 const getStart = async () => {
+  try {
+    await client.connect();
 
-  try{
-   await client.connect();
-
-   const collection =  await client.db("test").collection("garages");
-   return await collection.find().toArray();
-
-  }catch (e){
-      console.log('ошибка соединения', e);
+    const collection = await client.db('test').collection('garages');
+    return await collection.find().toArray();
+  } catch (e) {
+    console.log('ошибка соединения', e);
   }
+};
 
-}
+const postStart = async (newObject) => {
+  try {
+    await client.connect();
 
-const postStart = async () => {
-
-    try{
-        await client.connect();
-
-        const collection =  await client.db("test").collection("garages");
-        return await collection.find().toArray();
-
-    }catch (e){
-        console.log('ошибка соединения', e);
-    }
-
-}
+    const collection = await client.db('test').collection('garages');
+    await collection.insertOne(newObject);
+    console.log('подключено');
+  } catch (e) {
+    console.log('ошибка соединения', e);
+  }
+};
 
 @Controller('api')
 export class GarageController {
-
   @Get('/garages')
   getWorld() {
-
     return getStart();
   }
 
   @Post('/garages')
-    postWorld(@Res() res) {
-
-       console.log('res', res)
-    }
+  postWorld(@Req() req) {
+    postStart(req.body);
+  }
 }
